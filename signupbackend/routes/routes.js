3b931f34c12cf96b38129bbc0a808e2c997db5ba
httpcyberdata <borgeringstad@Brge-Ringstad-PC.local>
2022-11-09 22:08:00 -0500

    added bcrypt for hashing password

--- signupbackend/routes/routes.js
+++ signupbackend/routes/routes.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const router = express.Router()
 const signUpTemplateCopy = require('../models/SignUpModels');
+const bcrypt = require('bcrypt');
+router.post('/signup', async (req, res) => {
 
-router.post('/signup', (req, res) => {
+    const saltPassword = await bcrypt.genSalt(10)
+    const securePassword = await bcrypt.hash(req.body.password, saltPassword)
+
     const signedUpUser = new signUpTemplateCopy({
         fullName: req.body.fullName,
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password
+        password: securePassword
     })    
     signedUpUser.save()
     .then(save => {
