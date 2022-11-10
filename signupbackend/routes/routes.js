d79ea8e0fb473b36e381ba00d5347f02d7f86376
httpcyberdata <borgeringstad@Brge-Ringstad-PC.local>
2022-11-09 21:52:46 -0500

    added form onSubmit event handler and added axios + post form

--- signupbackend/routes/routes.js
+++ signupbackend/routes/routes.js
@@ -3,7 +3,19 @@ const router = express.Router()
 const signUpTemplateCopy = require('../models/SignUpModels');
 
 router.post('/signup', (req, res) => {
-    
+    const signedUpUser = new signUpTemplateCopy({
+        fullName: req.body.fullName,
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })    
+    signedUpUser.save()
+    .then(save => {
+        res.json(data);
+    })
+    .catch(error => {
+        res.json(error)
+    })
 })
-/**test */
+
 module.exports = router;
